3e258779c4f837ff2498eaf059bcf68ad45d6db7
gfedorov7 <fedorov.programming@gmail.com>
2025-07-18 00:10:43 +0300

    add exception handler

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from src.exceptions import AppException
 
 
 app = FastAPI()
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message},
+    )
